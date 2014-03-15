c7a1698336c9845e6cf27f7ac500c04a0ac7d2e0
balajiathreya <athreya86@gmail.com>
2014-03-15 02:21:43 -0400

    about me page

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,8 +5,11 @@ from __future__ import unicode_literals
 AUTHOR = u'Balaji Athreya'
 SITENAME = u'Balaji Athreya'
 SITEURL = 'balajiathreya.com'
+PATH = '/home/balaji/personalwebsite/content'
 
-TIMEZONE = 'Europe/Paris'
+
+
+TIMEZONE = 'America/Tijuana'
 
 DEFAULT_LANG = u'en'
 
@@ -16,18 +19,26 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS =  (('Resume', 'https://docs.google.com/document/d/14PTi6HTlyhq-_eKqlyozZWhLmQilcbJK99KmNX9TdiA/edit?usp=sharing'),)
 
 # Social widget
 SOCIAL = (('Twitter', 'https://twitter.com/athreya86'),
-          ('Linkedin', 'http://www.linkedin.com/in/balajiathreya/'),)
+          ('Linkedin', 'http://www.linkedin.com/in/balajiathreya/'),
+	  ('Goodreads', 'https://www.goodreads.com/user/show/25875081-balaji-athreya'),
+	  ('Github', 'https://github.com/balajiathreya'))
+
+
+#DISPLAY_PAGES_ON_MENU = False
+#DISPLAY_CATEGORIES_ON_MENU = False
+#DISPLAY_TAGS_ON_SIDEBAR = False
+
 
 DEFAULT_PAGINATION = 10
 
-THEME="../pelican-themes/gum"
+THEME="../pelican-themes/pelican-bootstrap3"
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+
+#GITHUB_USER="balajiathreya"
